58731beca7850b60e7720f011c1f472ec189710f
“Nguirane120” <badaranguirane120@gmail.com>
2024-02-07 15:43:13 +0000

    footer section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Destinations from './components/Destinations';
 import Search from './components/Search';
 import Selects from './components/Selects';
 import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
     <Search/>
     <Selects/>
     <Carousel/>
+    <Footer/>
     </>
   );
 }
